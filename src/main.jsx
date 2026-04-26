da06885975117c2f38ee5e377781896de3cd96ae
Juan Camilo Grajales B <juangrajales305127@correo.itm.edu.co>
2026-04-25 21:19:41 -0500

    feat: pagina de prueba Firestore

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './pages/Login.jsx';
-import Registro from './pages/Registro.jsx';
+import FirebaseTest from './pages/FirebaseTest.jsx';
+
+const basePath = import.meta.env.BASE_URL.replace(/\/$/, '');
+const currentPath = window.location.pathname.replace(basePath, '') || '/';
+const CurrentPage = currentPath === '/test' ? FirebaseTest : Login;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
-    {/*<Registro />  */} 
+    <CurrentPage />
   </StrictMode>,
 )
