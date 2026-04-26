a5a350c5aa2d0f279d7f033ae5fb12d3471b1254
Juan Camilo Grajales B <juangrajales305127@correo.itm.edu.co>
2026-04-25 23:23:51 -0500

    chore: elimina pagina de prueba Firestore

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './pages/Login.jsx';
-import FirebaseTest from './pages/FirebaseTest.jsx';
-
-const basePath = import.meta.env.BASE_URL.replace(/\/$/, '');
-const currentPath = window.location.pathname.replace(basePath, '') || '/';
-const CurrentPage = currentPath === '/test' ? FirebaseTest : Login;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CurrentPage />
+    <Login />
   </StrictMode>,
 )
